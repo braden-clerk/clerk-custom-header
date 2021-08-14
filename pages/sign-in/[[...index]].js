26baec5c1ffab1c720191f13bb9babd5b80f3a39
braden-clerk <braden@clerk.dev>
2021-08-13 18:06:23 -0600

    Add custom header

--- pages/sign-in/[[...index]].js
+++ pages/sign-in/[[...index]].js
@@ -1,7 +1,12 @@
 import { SignIn } from "@clerk/clerk-react";
 
 const SignInPage = () => (
-  <SignIn path="/sign-in" routing="path" />
+  <>
+    <div className="cl-wrapper">
+      <div className="cl-custom-header">You need to sign in to continue</div>
+      <SignIn path="/sign-in" routing="path" />
+    </div>
+  </>
 );
 
 export default SignInPage;
--- pages/sign-up/[[...index]].js
+++ pages/sign-up/[[...index]].js
@@ -1,7 +1,10 @@
 import { SignUp } from "@clerk/clerk-react";
 
 const SignUpPage = () => (
-  <SignUp path="/sign-up" routing="path" />
+  <div className="cl-wrapper">
+    <div className="cl-custom-header">You need to sign up to continue</div>
+    <SignUp path="/sign-up" routing="path" />
+  </div>
 );
 
 export default SignUpPage;
